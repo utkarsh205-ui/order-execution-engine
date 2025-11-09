1ab9cd83c1c9097552cff0a2fbcbddc6d6d5f100
Utkarsh Dhilliwal <128224432+utkarsh205-ui@users.noreply.github.com>
2025-11-09 17:00:31 +0530

    Remove commented setupFilesAfterEnv line
    Removed commented setupFilesAfterEnv configuration.

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,7 +2,6 @@ export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   clearMocks: true,
-  // DELETE THIS LINE: setupFilesAfterEnv: ['./jest.setup.ts'],
   
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
